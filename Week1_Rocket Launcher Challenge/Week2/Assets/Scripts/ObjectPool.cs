a79533960d4678c90fe494518e9e50d65ddb172e
pchanbro <cksgud0509@naver.com>
2024-10-24 23:02:52 +0900

    Feat : ObjectPool Code

---
+++ Week1_Rocket Launcher Challenge/Week2/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    private List<GameObject> pool;
+    private const int minSize = 50;
+    private const int maxSize = 300;
+
+    public GameObject objectPrefab;
+
+    void Awake()
+    {
+        pool = new List<GameObject>();
+        for (int i = 0; i < minSize; i++)
+        {
+            pool.Add(CreateObject());
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.V))
+        {
+            CreateObject();
+            Debug.Log($"현재 오브젝트 {pool.Count}개");
+        }
+    }
+
+    private GameObject CreateObject()
+    {
+        // [요구스펙 1] Create Object
+        GameObject newObj = Instantiate(objectPrefab);
+        newObj.SetActive(false);
+        return newObj;
+    }
+
+    public GameObject GetObject()
+    {
+        // [요구스펙 2] Get Object
+    }
+
+    public void ReleaseObject(GameObject obj)
+    {
+        // [요구스펙 3] Release Object
+    }
+
+}
